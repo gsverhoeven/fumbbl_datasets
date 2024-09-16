eeda8be0fc3f7ad2d0909a7ee7d1dd345d642655
gsverhoeven <gertjan.verhoeven@gmail.com>
2024-09-16 08:43:35 +0200

    add caching to scrape html function, only fetch if not present

--- src/scrape_match_htmls.py
+++ src/scrape_match_htmls.py
@@ -11,24 +11,35 @@ def scrape_match_htmls(full_run, begin_match, end_match, verbose = False):
 
     if(full_run):
         for i in range(n_matches):
+
             match_id = end_match - i
-            api_string = "https://fumbbl.com/FUMBBL.php?page=match&id=" + str(match_id)
-
-            response = requests.get(api_string)
-
             dirname = "raw/match_html_files/" + str(match_id)[0:4]
-            if not os.path.exists(dirname):
-                os.makedirs(dirname)
-
             fname_string = dirname + "/match_" + str(match_id) + ".html.gz"
-            
-            with gzip.open(fname_string, mode = "wb") as f:
-                f.write(response.text.encode("utf-8"))
-                f.close()
 
-            
+            # check if file already exists, else scrape it
+            try:
+                f = open(fname_string, mode = "rb")
+
+            except OSError as e:
+                if not os.path.exists(dirname):
+                    os.makedirs(dirname)
+                # fetch html
+                api_string = "https://fumbbl.com/FUMBBL.php?page=match&id=" + str(match_id)
+
+                response = requests.get(api_string)
+                # write html
+                with gzip.open(fname_string, mode = "wb") as f:
+                    f.write(response.text.encode("utf-8"))
+                    f.close()
+                    if verbose:
+                        print("o", end = '')
+            else:
+                # file already present
+                pass
+
             if i % 1000 == 0: 
                 if verbose:
                 # write progress report
-                    print(i, end='')
-                    print(".", end='')
+                    print(i, end = '')
+                    print(".", end = '')
+
